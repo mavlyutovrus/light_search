fc73050c8a366712a847ed6ec01a077953f42695
Ruslan Mavlyutov <m-ceros@yandex.ru>
2015-10-20 18:56:42 +0200

    warmup query index output

--- warmup.py
+++ warmup.py
@@ -22,7 +22,10 @@ for key, values in search_engine.word_index.keys_db.items():
 top_freq.sort(reverse=True)
 top_freq = top_freq[:1005]
 import urllib2
+index = 0
 for _, token in top_freq:
     json_val = urllib2.urlopen("http://" + MACHINE_NETWORK_NAME + ":" + str(port) + "/?q=" + token + "&json=1").read()
     print json_val
+    print "INdex::", index
+    index += 1
 
